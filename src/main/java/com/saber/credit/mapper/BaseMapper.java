540577009af0638efab045b08c67492b9c9494a7
Administrator <639878266@qq.com>
2019-06-11 23:11:22 +0800

    1.设计产品表

--- src/main/java/com/saber/credit/mapper/BaseMapper.java
+++ src/main/java/com/saber/credit/mapper/BaseMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 /**
  * Created by saber on 2019/6/3
+ * 所有mapper层对象可以继承该抽象接口实现基本的增删改操作
  */
 public interface BaseMapper<T> {
     /**
@@ -14,7 +15,7 @@ public interface BaseMapper<T> {
     /**
      * @param page page表示当前页数
      * @param limit limit表示用户设置的每页显示多少条数据
-     * @return
+     * @return 对象集合
      */
     List<T> query(Integer page, Integer limit);
     /**
@@ -28,7 +29,7 @@ public interface BaseMapper<T> {
     void deleteById(String id);
     /**
      * 通过id修改数据
-     * @return 该id对应的数据
+     * @param id 该id对应的数据
      */
     void updateById(String id);
 }
