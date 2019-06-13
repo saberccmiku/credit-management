71b401918596c90c11cc3cb5c817ea6b3d9c98eb
Administrator <639878266@qq.com>
2019-06-13 23:29:49 +0800

    1.添加产品列表

--- src/main/java/com/saber/credit/controller/ProductController.java
+++ src/main/java/com/saber/credit/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.saber.credit.controller;
 
+import com.saber.credit.entities.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by saber on 2019/6/12.
@@ -22,6 +24,14 @@ public class ProductController extends BaseController{
         initPage(model);
         initEnumerate(model,"SETTLEMENT_CYCLE","CREDIT_CYCLE","ACCOUNT_WAY","REPAYMENT_WAY","CREDIT_MATERIAL_TYPE","CREDIT_INFO","REVIEW_WAY","PREPAYMENT","VERIFY_WAY");
         return "product/add";
+    }
+
+    @PostMapping(value = "/product")
+    public String saveProduct(Product product){
+        System.out.println("---------------------------------");
+        System.out.println(product);
+        System.out.println("---------------------------------");
+        return "redirect:/product/products";
     }
 
 }
