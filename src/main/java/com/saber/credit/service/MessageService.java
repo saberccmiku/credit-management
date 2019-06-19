e74233ec27b7c2516e2ae8e06d7789874a5fb3ca
saber <639878266@qq.com>
2019-06-19 18:18:55 +0800

    1.完善了产品列表信息

---
+++ src/main/java/com/saber/credit/service/MessageService.java
@@ -0,0 +1,10 @@
+package com.saber.credit.service;
+
+import com.saber.credit.entities.Message;
+
+/**
+ * Created by saber on 2019/6/19.
+ * 通信消息
+ */
+public interface MessageService extends BaseService<Message> {
+}
---
+++ src/main/java/com/saber/credit/service/impl/MessageServiceImpl.java
@@ -0,0 +1,13 @@
+package com.saber.credit.service.impl;
+
+import com.saber.credit.entities.Message;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by saber on 2019/6/19.
+ * 通知消息
+ */
+@Service
+public class MessageServiceImpl extends BaseServiceImpl<Message> {
+
+}
