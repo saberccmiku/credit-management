a21801439506305117428c9ef26b15ac37524a29
Administrator <639878266@qq.com>
2019-06-20 23:44:13 +0800

    1.修改消息通知

--- src/main/java/com/saber/credit/controller/user/MessageController.java
+++ src/main/java/com/saber/credit/controller/user/MessageController.java
@@ -1,11 +1,17 @@
 package com.saber.credit.controller.user;
 
 import com.saber.credit.controller.BaseController;
+import com.saber.credit.entities.Message;
+import com.saber.credit.service.impl.MessageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,18 +21,31 @@ import java.util.Map;
 @Controller
 public class MessageController extends BaseController {
 
+
+    @Autowired
+    private MessageServiceImpl messageService ;
+
     @GetMapping(value = "user/message")
-    public String List(Model model){
+    public String list(Model model){
+        List<Message> messages = messageService.query(1, 20);
+        model.addAttribute("messages",messages);
         initPage(model);
         initEnumerate(model,"MESSAGE_TYPE");
         return "user/message";
     }
 
     @PostMapping(value = "user/sendMessage")
-    public String sendMessage(Model model, Map<String,Object> map){
+    public String sendMessage(Message message,Model model, Map<String,Object> map){
+        messageService.insert(message);
         map.put("msg","success");
         initPage(model);
-        initEnumerate(model,"MESSAGE_TYPE");
-        return "user/message";
+        return list(model);
+    }
+
+    @DeleteMapping(value = "/message/{id}")
+    public String deleteMessage(@PathVariable("id") String id,Model model,Map<String,Object> map){
+        messageService.delete(id);
+        map.put("msg","success");
+        return list(model);
     }
 }
